7f3d9254efb41dd451a463b30793fb03089ebcae
Jake O´Donnell <jakebtodonnell@gmail.com>
2020-03-09 12:07:08 +0100

    img size

--- Drinking game/Views/pestellerkolera.swift
+++ Drinking game/Views/pestellerkolera.swift
@@ -29,7 +29,11 @@ class pestellerkolera: UIViewController {
     var pestFraga = ["1","2","3","4","5","6","7","8","9","10","11","12"]
     var koleraFraga = ["1","2","3","4","5","6","7","8","9","10","11","12"]
 
-   
+    /*
+    let pestFraga = ["Aldrig mer åka buss","Smaklökar i anus","Alltid uppklädd i finkläder", "Få tio miljoner kronor om tio år", "Vakna upp varje morgon insmetad i gröt", "Kunna gå på vatten", "Alltid glömma dina lösenord", "Endast kunna prata och förstå finska i ett år ", "Raka av dina ögonbryn", "Gå in på ett gym och påpeka hur svaga alla är","11"]
+    
+    let koleraFraga = ["Aldrig mer åka tåg","Stoppa ner dina fingrar i en köttkvarn","alltid iklädd mjuksbyxor och en skön tröja", "Få fem miljoner kronor idag", "Vakna upp varje morgon utanför din ytterdörr", "Kunna andas under vattnet", "Alltid glömma vad dina vänner heter", "Alltid bryta på finska", "Att dina ögonfransar ramlar av", "gå naken på ett spinningpass?","11"]
+    */
     @IBOutlet weak var counterQuestion: UILabel!
      let wallpapers = ["wallpaper/1.png", "wallpaper/2.png", "wallpaper/3.png", "wallpaper/4.png", "wallpaper/5.png", "wallpaper/6.png", "wallpaper/7.png", "wallpaper/8.png", "wallpaper/9.png", "wallpaper/10.png", "wallpaper/11.png", "wallpaper/12.jpg", "wallpaper/13.jpg", "wallpaper/14.jpg", "wallpaper/15.jpg", "wallpaper/16.jpg", "wallpaper/17.jpg", "wallpaper/18.jpg", "wallpaper/19.jpg"]
     /*
